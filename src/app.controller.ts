ef2b918501473b737d6ddf79e4482f600c369d07
eddymandran <eddy.mandran@gmail.com>
2022-09-12 12:05:00 +0200

    routing decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import {Controller, Get} from '@nestjs/common'
 
-@Controller()
+// to get access to the request object : localhost:3000/app/hi or localhost:3000/app/bye
+@Controller('/app')
 export class AppController {
 
-    @Get()
+    @Get('/hi')
     getRootRoute() {
         return "Hi there!";
+    }
+
+    @Get('/bye')
+    getByeThere() {
+        return "Bye there!";
     }
 }
