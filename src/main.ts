1782df72db8a3f7c10a43d030dddd6a3053ffeab
eddymandran <eddy.mandran@gmail.com>
2022-09-12 11:45:12 +0200

    srating a up a Nest app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 @Controller()
 class AppController {
@@ -8,3 +9,17 @@ class AppController {
         return "Hi there!";
     }
 }
+
+@Module({
+    controllers: [AppController]
+})
+class AppModule {
+
+}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+}
+
+bootstrap();
